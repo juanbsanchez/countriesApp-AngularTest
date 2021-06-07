22c10d5d275ca6b86c6a6e77b2b0f3371a5d3f0a
juanbsanchez <juanbsanchez1988@gmail.com>
2021-06-07 19:26:31 +0200

    config routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import {SharedModule} from "./shared/shared.module";
 import {CountryModule} from "./country/country.module";
+import {AppRoutingModule} from "./app-routing.module";
+import {RouterModule} from "@angular/router";
 
 @NgModule({
   declarations: [
@@ -11,8 +13,10 @@ import {CountryModule} from "./country/country.module";
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     SharedModule,
-    CountryModule
+    CountryModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
