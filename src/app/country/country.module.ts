dff5b5ecb19029a4de9b77d4efe0967fee5d61b9
juanbsanchez <juanbsanchez1988@gmail.com>
2021-06-08 19:29:13 +0200

    basic structuring of by-country.component

--- src/app/country/country.module.ts
+++ src/app/country/country.module.ts
@@ -4,6 +4,7 @@ import { ByCapitalComponent } from './pages/by-capital/by-capital.component';
 import { ByCountryComponent } from './pages/by-country/by-country.component';
 import { ByRegionComponent } from './pages/by-region/by-region.component';
 import { ShowCountryComponent } from './pages/show-country/show-country.component';
+import {FormsModule} from "@angular/forms";
 
 
 
@@ -15,7 +16,8 @@ import { ShowCountryComponent } from './pages/show-country/show-country.componen
     ShowCountryComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ],
   exports: [
     ByCapitalComponent,
