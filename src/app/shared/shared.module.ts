bdadfa028bdf47e347db1d3e645c20c2077d3574
juanbsanchez <juanbsanchez1988@gmail.com>
2021-06-08 08:53:07 +0200

    refactor app.component and sidebar.component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SidebarComponent } from './sidebar/sidebar.component';
+import {RouterModule} from "@angular/router";
 
+
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 
 @NgModule({
@@ -9,7 +11,8 @@ import { SidebarComponent } from './sidebar/sidebar.component';
     SidebarComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports:[
     SidebarComponent
