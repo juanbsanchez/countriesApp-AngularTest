18b3b1ff1eb76f0ff4149026b59a4376b7b6c87f
juanbsanchez <juanbsanchez1988@gmail.com>
2021-06-11 20:16:45 +0200

    implement activateRegion methods and call service to get countries by region

--- src/app/country/pages/by-region/by-region.component.ts
+++ src/app/country/pages/by-region/by-region.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {CountryService} from "../../services/country.service";
+import {Country} from "../../interfaces/country.interface";
 
 @Component({
   selector: 'app-by-region',
@@ -15,17 +17,35 @@ export class ByRegionComponent implements OnInit {
 
   regions: string[] = ['africa', 'americas', 'asia', 'europe', 'oceania'];
   activeRegion: string = '';
+  countries: Country[] = [];
 
-  constructor() { }
+  constructor(private countryService: CountryService) { }
 
   ngOnInit(): void {
+
+  }
+
+  getCssClass (region: string) {
+    return (region === this.activeRegion) ? 'btn btn-primary' : 'btn btn-outline-primary';
   }
 
   activateRegion(region: string){
 
-    this.activeRegion = region;
+    if(region === this.activeRegion){return;}
 
-    //TODO: call service
+    this.activeRegion = region;
+    this.countries = [];
+
+    this.countryService.getCountryByRegion(this.activeRegion)
+      .subscribe(
+        (countries) => {
+          this.countries = countries;
+
+        },
+        error => {
+          console.log(error);
+        }
+      )
   }
 
 }
