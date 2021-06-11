d20abd673ff0cee29138d78a47f9dc71099f7714
juanbsanchez <juanbsanchez1988@gmail.com>
2021-06-11 18:18:45 +0200

    basic layout of by-region.component

--- src/app/country/pages/by-region/by-region.component.ts
+++ src/app/country/pages/by-region/by-region.component.ts
@@ -4,13 +4,28 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-by-region',
   templateUrl: './by-region.component.html',
   styles: [
+    `
+    button{
+      margin-right: 5px;
+    }
+    `
   ]
 })
 export class ByRegionComponent implements OnInit {
 
+  regions: string[] = ['africa', 'americas', 'asia', 'europe', 'oceania'];
+  activeRegion: string = '';
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  activateRegion(region: string){
+
+    this.activeRegion = region;
+
+    //TODO: call service
   }
 
 }
